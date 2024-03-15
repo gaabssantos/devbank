0c6d6cb591ab1bea2d1a8fe592fedc64612eaf1e
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-03-15 13:13:42 -0300

    Revert "feat(MongoDB connected succesful): :sparkles:"
    This reverts commit 50aca766f0125678c02ce65e689a9b8c32aacfd4.

--- devbank-server/src/server.ts
+++ devbank-server/src/server.ts
@@ -1,16 +1,13 @@
 import express from 'express';
 
 import 'dotenv/config';
-import { initMongo } from './database';
 import { routes } from './routes';
 
-initMongo().then(() => {
-  const app = express();
+const app = express();
 
-  app.use(express.json());
-  app.use(routes);
+app.use(express.json());
+app.use(routes);
 
-  app.listen(process.env.PORT, () => {
-    console.log('🚀 Server has started!');
-  });
+app.listen(process.env.PORT, () => {
+  console.log('🚀 Server has started!');
 });
