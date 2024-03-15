50aca766f0125678c02ce65e689a9b8c32aacfd4
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-03-15 11:53:10 -0300

    feat(MongoDB connected succesful): :sparkles:

--- devbank-server/src/server.ts
+++ devbank-server/src/server.ts
@@ -1,13 +1,16 @@
 import express from 'express';
 
 import 'dotenv/config';
+import { initMongo } from './database';
 import { routes } from './routes';
 
-const app = express();
+initMongo().then(() => {
+  const app = express();
 
-app.use(express.json());
-app.use(routes);
+  app.use(express.json());
+  app.use(routes);
 
-app.listen(process.env.PORT, () => {
-  console.log('🚀 Server has started!');
+  app.listen(process.env.PORT, () => {
+    console.log('🚀 Server has started!');
+  });
 });
