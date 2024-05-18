a316bd88cd6fcee123c5ab5cced097e8612acd53
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-05-18 15:07:31 -0300

    feat(Auth token middleware in transfer): :sparkles:

--- devbank-server/src/routes/index.ts
+++ devbank-server/src/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 
+import authMiddleware from '../middlewares/auth.middleware';
 import { mainRoute } from './main.route';
 import { transferRoute } from './transfers.route';
 import { userRoute } from './users.route';
@@ -8,4 +9,5 @@ export const routes = Router();
 
 routes.use('/', mainRoute);
 routes.use('/users', userRoute);
+routes.use(authMiddleware);
 routes.use('/transfers', transferRoute);
