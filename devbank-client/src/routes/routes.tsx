4f8f167e1d91101956f235fb934d8c183310bf88
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-06-28 16:28:17 -0300

    refactor(New route): :recycle:

--- devbank-client/src/routes/routes.tsx
+++ devbank-client/src/routes/routes.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 
 import DefaultLayout from '../layouts/Layout';
+import Account from '../screens/Account';
 import Home from '../screens/Home';
 import Login from '../screens/Login';
 import SignUp from '../screens/SignUp';
@@ -12,6 +13,7 @@ const Router = () => {
         <Route path="/" element={<Home />} />
         <Route path="/cadastrar" element={<SignUp />} />
         <Route path="/logar" element={<Login />} />
+        <Route path="/account" element={<Account />} />
       </Route>
     </Routes>
   );
