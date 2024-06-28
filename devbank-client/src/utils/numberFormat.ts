470716f3b18e8b64b3687aaa1c5e325dbe824b0d
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-06-28 17:24:09 -0300

    refactor(New function for format numbers): :sparkles:

---
+++ devbank-client/src/utils/numberFormat.ts
@@ -0,0 +1,8 @@
+const numberFormat = (value: number) => {
+  return new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(value);
+};
+
+export default numberFormat;
